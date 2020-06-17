e179b5db9f36b7aeb77ff33d802cd99196aab348
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-06-17 13:13:04 +0900

    Split fetch

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -103,31 +103,3 @@ fn main() {
 
     println!("result = {}", &ram[64]);
 }
-
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-pub fn read_rom_lines(filename: &str) {
-    if let Ok(lines) = read_lines(filename) {
-        for line in lines {
-            if let Ok(ip) = line {
-                let instructions: Vec<&str> = ip
-                    .split(|c| c == ' ' || c == ',')
-                    .filter(|&s| s != "")
-                    .collect();
-                for inst in instructions {
-                    print!("{} ", inst);
-                }
-            }
-        }
-    }
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
